39e959d91ee816731cb583357c66c6da90514943
Valentine Radchuk <(null)>
2014-11-25 23:23:20 +0200

    Add support of SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -5,61 +5,13 @@ var webserver = require('gulp-webserver');
 var gutil = require('gulp-util');
 var fs = require('fs');
 var prettify = require('gulp-prettify');
+var sass = require('gulp-sass');
 
-gulp.task('default',['watchHtml', 'webserver']);
 
-// build pages for HW2
-var outputDir = './build';
-var hwDir = './src/';
-var headerPath = hwDir + 'static/header.html';
-var footerPath = hwDir + 'static/footer.html';
+gulp.task('default',['webserver','watchSass']);
 
-gulp.task('hw2ConcatIndex', function(){
-	concatPage('index');
-});
-
-gulp.task('hw2ConcatAbout', function(){
-	concatPage('about');
-});
-
-gulp.task('hw2ConcatFaq', function(){
-	concatPage('faq');
-});
-
-gulp.task('hw2ConcatFeedback', function(){
-	concatPage('feedback');
-});
-
-gulp.task('hw2ConcatContacts', function(){
-	concatPage('contacts');
-});
-
-gulp.task('hw2ConcatNews', function(){
-	concatPage('news');
-});
-
-gulp.task('hw2ConcatRegister', function(){
-	concatPage('register');
-});
-
-function concatPage(fileName){
-	console.log(fileName);
-	
-	var contentFilePath = hwDir + fileName + '_src.html';
-	console.log(contentFilePath);
-	gulp.src([headerPath, contentFilePath,footerPath])
-		.pipe(concat(fileName + '.html'))
-		.pipe(gulp.dest(outputDir));
-};
-
-gulp.task('watchHtml', function() {
-	gulp.watch(hwDir + '**/*.html',['hw2ConcatIndex','hw2ConcatAbout','hw2ConcatFaq','hw2ConcatFeedback','hw2ConcatContacts','hw2ConcatNews','hw2ConcatRegister', 'prettifyHtml']);
-});
-
-gulp.task('prettifyHtml', function() {
-  gulp.src(outputDir + '/*.html')
-    .pipe(prettify({indent_size: 4}))
-    .pipe(gulp.dest('./'))
+gulp.task('watchSass', function() {
+	gulp.watch('./scss/*.scss',['sass']);
 });
 
 //end build pages for HW2
@@ -74,3 +26,16 @@ gulp.task('webserver', function() {
       		open: true
 		}));
 });
+
+gulp.task('sass', function(){
+	gulp.src('./scss/*.scss')
+        .pipe(sass())
+        .on('error', swallowError)
+        .pipe(gulp.dest('./css'));
+	});
+
+function swallowError (error) {
+    //If you want details of the error in the console
+    console.log(error.toString());
+    this.emit('end');
+}
