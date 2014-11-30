6339aa43a1a33d01665c46040c5add7d13437720
Valentine Radchuk <(null)>
2014-11-30 16:28:50 +0200

    Working on header

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,10 @@ var prettify = require('gulp-prettify');
 var sass = require('gulp-sass');
 
 
-gulp.task('default',['webserver','watchSass']);
+gulp.task('default', ['webserver', 'watchSass']);
 
 gulp.task('watchSass', function() {
-	gulp.watch('./scss/*.scss',['sass']);
+	gulp.watch('./scss/*.scss', ['sass']);
 });
 
 //end build pages for HW2
@@ -20,22 +20,22 @@ gulp.task('webserver', function() {
 	gulp.src('./')
 		.pipe(webserver({
 			livereload: true,
-      		directoryListing: { 
-      			enable: true,
-      		},
-      		open: true
+			directoryListing: {
+				enable: true,
+			},
+			open: true
 		}));
 });
 
-gulp.task('sass', function(){
+gulp.task('sass', function() {
 	gulp.src('./scss/*.scss')
-        .pipe(sass())
-        .on('error', swallowError)
-        .pipe(gulp.dest('./css'));
-	});
+		.pipe(sass())
+		.on('error', swallowError)
+		.pipe(gulp.dest('./css'));
+});
 
-function swallowError (error) {
-    //If you want details of the error in the console
-    console.log(error.toString());
-    this.emit('end');
+function swallowError(error) {
+	//If you want details of the error in the console
+	console.log(error.toString());
+	this.emit('end');
 }
